e67ffb3273dd72c12b54fb0d946315ae4993dcc8
anaibanez <ana1106@gmail.com>
2018-04-04 09:54:45 +0200

    clientList name

--- client-management-frontend/src/containers/ClientListPage/saga/rootSaga.js
+++ client-management-frontend/src/containers/ClientListPage/saga/rootSaga.js
@@ -4,7 +4,7 @@ import getDataTable from './getDataTableSaga';
 import deleteClient from './deleteClientSaga';
 import { GET_DATA_TABLE, DELETE_CLIENT } from '../constants';
 
-export default function* product() {
+export default function* clientList() {
   yield takeLatest(GET_DATA_TABLE, getDataTable);
   yield takeLatest(DELETE_CLIENT, deleteClient);
 }
